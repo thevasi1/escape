65828929547eb6c8c60e05fcf1b87a76936c7e24
thevasi1 <thevasi1@abv.bg>
2023-06-26 00:32:08 +0300

    add register functionality

--- frontend/pages/login/login.js
+++ frontend/pages/login/login.js
@@ -3,7 +3,6 @@ function login() {
     const password = document.getElementById('login-password').value;
 
     checkLoginData(email, password).then(message => {
-        console.log(message);
         if (message["status"] != "200") {
             throw new Error(message["message"]);
           }
@@ -13,8 +12,7 @@ function login() {
     })
 }
 
-async function checkLoginData(email, password) {
-    console.log(email, password);
+function checkLoginData(email, password) {
     const headers = { "Content-Type": "application/json" };
     const init = {
         method: "POST",
@@ -22,7 +20,33 @@ async function checkLoginData(email, password) {
         body: JSON.stringify({"email-input": email, "password-input": password})
       };
   return fetch("../../../backend/api/login/login-user.php", init).then(res => {
-	  console.log(res);
+	  return res;
+  })
+}
+
+function register() {
+    const username = document.getElementById('register-username').value;
+    const email = document.getElementById('register-email').value;
+    const password = document.getElementById('register-password').value;
+
+    checkLoginData(username, email, password).then(message => {
+        if (message["status"] != "200") {
+            throw new Error(message["message"]);
+          }
+          else {
+            window.location.replace("../form/form.html");
+          }
+    })
+}
+
+function checkLoginData(username, email, password) {
+    const headers = { "Content-Type": "application/json" };
+    const init = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({"username": username, "email-register": email, "password-register": password})
+      };
+  return fetch("../../../backend/api/register/register.php", init).then(res => {
 	  return res;
   })
 }
