4cffd1c0ac8386fe61d81ae5bc9b1029e63783d7
thevasi1 <thevasi1@abv.bg>
2023-06-25 21:23:06 +0300

    test login function

--- frontend/pages/login/login.js
+++ frontend/pages/login/login.js
@@ -1,3 +1,18 @@
 function login() {
+    checkLoginData();
     window.location.replace("../form/form.html");
+}
+
+async function checkLoginData() {
+    const data = {email: "guzami@at.me", password:"0000"}
+    const headers = { "Content-Type": "application/json" };
+    const init = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data)
+      };
+  const res = await fetch("../../../backend/api/login/login_user.php", init);
+  const jsonRes = JSON.parse(res);
+  console.log(jsonRes);
+  return jsonRes;
 }
