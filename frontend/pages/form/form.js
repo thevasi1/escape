25f125e0d295f560a0c7cf3af5e5843a671579fa
thevasi1 <thevasi1@abv.bg>
2023-06-27 02:24:49 +0300

    add reactive data flow for form inouts

--- frontend/pages/form/form.js
+++ frontend/pages/form/form.js
@@ -1,15 +1,32 @@
 const arr = [1, 2, 3];
+let lastRoomIndex = -1;
+const obj = {
+    tasks: [
+        {
+            type: 'numeric',
+            solution: '6-9',
+            message: 'qj mi guza'
+        },
+        {
+            type: 'text',
+            solution: '4-2-0',
+            message: 'nqma ti qm guza'
+        },
+        {
+            type: 'direction',
+            solution: '9-6',
+            message: 'ma moa probvam'
+        },
+        {
+            type: 'shape',
+            solution: '9-6-0',
+            message: 'zaaaebi'
+        }
+    ]
+}
 window.addEventListener("load", function () {
     const tabs = this.document.getElementById('tabs');
-    arr.forEach((item, i) => {
-        const newItem = document.createElement('li');
-        const newAnchor = document.createElement('a');
-        newAnchor.textContent = `Room ${i}`;
-        newAnchor.setAttribute('href', `#tab-${i}`);
-        newItem.appendChild(newAnchor);
-
-        tabs.appendChild(newItem);
-    })
+    obj.tasks.forEach((item, i) => addRoom(item, i));
     const myTabs = document.querySelectorAll("ul.nav-tabs > li");
 
     function myTabClicks(tabClickEvent) {
@@ -19,20 +36,68 @@ window.addEventListener("load", function () {
         let clickedTab = tabClickEvent.currentTarget;
         clickedTab.classList.add("active");
         tabClickEvent.preventDefault();
-        let myContentPanes = document.querySelectorAll(".tab-pane");
-        for (i = 0; i < myContentPanes.length; i++) {
-            myContentPanes[i].classList.remove("active");
-        }
-        let anchorReference = tabClickEvent.target;
-        let activePaneId = anchorReference.getAttribute("href");
-        let activePane = document.querySelector(activePaneId);
-        activePane.classList.add("active");
     }
 
     for (i = 0; i < myTabs.length; i++) {
         myTabs[i].addEventListener("click", myTabClicks)
     }
+
+    function addRoom(room, index) {
+        console.log(room);
+        const newItem = document.createElement('li');
+
+        const newAnchor = document.createElement('a');
+        newAnchor.textContent = `Escape ${index + 1}`;
+
+        newAnchor.setAttribute('href', `#tab-${index + 1}`);
+        newAnchor.setAttribute('data-key', index);
+
+        newItem.appendChild(newAnchor);
+
+        tabs.prepend(newItem);
+        newItem.addEventListener('click', clickRoom);
+    }
 });
+
+function clickRoom(event) {
+    const key = event.target.dataset.key;
+    loadParameters(key);
+}
+
+function loadParameters(index) {
+    const type = document.getElementById('contentType');
+    const solution = document.getElementById('contentSolution'); 
+    const message = document.getElementById('contentMessage');
+
+    if (lastRoomIndex != -1) {
+        obj.tasks[lastRoomIndex].type = type.value;
+        obj.tasks[lastRoomIndex].solution = solution.value;
+        obj.tasks[lastRoomIndex].message = message.value;
+    }
+
+    type.value = obj.tasks[index].type;
+    solution.value = obj.tasks[index].solution;
+    message.value = obj.tasks[index].message;
+    
+    lastRoomIndex = index;
+}
+
+function newRoom() {
+    const tabs = this.document.getElementById('tabs');
+    const newItem = document.createElement('li');
+
+    const newAnchor = document.createElement('a');
+    newAnchor.textContent = `Escape ${obj.tasks.length + 1}`;
+
+    newAnchor.setAttribute('href', `#tab-${obj.tasks.length + 1}`);
+    newAnchor.setAttribute('data-key', obj.tasks.length);
+
+    newItem.appendChild(newAnchor);
+
+    tabs.prepend(newItem);
+    newItem.addEventListener('click', clickRoom);
+    obj.tasks.push({});
+}
 
 function submit() {
     console.log("aaaa");
@@ -41,8 +106,9 @@ function submit() {
         'room_complexity': 3,
         'tasks': [
             {
-                'type': 'numberic',
-                'solution': '6-9'
+                type: 'numberic',
+                solution: '6-9',
+                message: 'qj mi guza'
             }
         ]
     };
